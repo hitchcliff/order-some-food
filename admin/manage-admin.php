2e4a19f322a05c831e1570e6746513b3650dd281
kevin700brands <kevin@700brands.com>
2022-08-24 16:52:52 +0800

    manage admin ui

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,6 +5,34 @@
 	<div class="wrapper">
 		<h2>Manage Admin</h2>
 
+		<br />
+		<button class="btn-primary">Add admin</button>
+		<br />
+		<br />
+
+		<table class="tbl-full">
+			<tr>
+				<th>
+					S.N.
+				</th>
+				<th>Full Name</th>
+				<th>Username</th>
+				<th>Actions</th>
+			</tr>
+			<tr>
+				<td>1.</td>
+				<td>Kevin Nacario</td>
+				<td>notkev1n</td>
+				<td>
+					<button class="btn-secondary">
+						Update Admin
+					</button>
+					<button class="btn-danger">
+						Delete Admin
+					</button>
+				</td>
+			</tr>
+		</table>
 	</div>
 </div>
 <!-- Main Content Section Ends-->
